9332b7a95f92f247da2116efe6774e4f18de73fd
rogercusco <rogercusco@gmail.com>
2016-05-18 10:46:55 +0200

    final changes to schema

--- scraper/Scraper.sql
+++ scraper/Scraper.sql
@@ -1,11 +1,17 @@
 CREATE DATABASE `scraper2` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `scraper2`;
 
+-- Create a table storing the outcodes scraped so far and a binary 
+-- indicating whether the scraping was succesfully completed.
+
 CREATE TABLE `outcode` (
   `id` int(11) NOT NULL,
   `completed` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Create main fact table storing the description and metadata for 
+-- each property.
 
 CREATE TABLE `property` (
   `id` int(11) NOT NULL,
@@ -22,6 +28,8 @@ CREATE TABLE `property` (
   CONSTRAINT `property_ibfk_1` FOREIGN KEY (`outcode_id`) REFERENCES `outcode` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Create a table storing the errors thrown during the process
+-- the object type that produced them and the error message.
 CREATE TABLE `error` (
   `obj_type` varchar(250) DEFAULT NULL,
   `id` varchar(20) DEFAULT NULL,
